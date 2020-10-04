05755902f0d88376fae7d8dd1eb83ad980ac65c9
Franck Kamenan <franck.kamenan@yahoo.com>
2020-10-04 12:38:02 +0200

    countBooksPerAuthor

--- BookApp.java
+++ BookApp.java
@@ -2,12 +2,15 @@ package be.intecbrussel.opdrachten.fridayopdracht;
 
 import java.time.*;
 import java.util.Comparator;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BookApp {
 
     public static void main(String[] args) {
         Book[] bookArray = new Book[5];
+        Person p1 = new Person("Kevin", "Grevioux", LocalDate.of(1962, 9, 9));
         bookArray[0] = new Book("Harry Potter and the Deathly Hallows", new Person("J.K",
                         "Rowling", LocalDate.of(1965, 7, 3)), LocalDate
                         .of(2020, 8, 8), "fantasy");
@@ -17,17 +20,17 @@ public class BookApp {
         bookArray[2] = new Book("Gangsta", new Person("Koshke", "Kosuke", LocalDate
                         .of(1974, 5, 5)), LocalDate.of(2014, 8, 27)
                         ,"seinen");
-        bookArray[3] = new Book("Blue Marvel", new Person("Kevin", "Grevioux", LocalDate
-                        .of(1962, 9, 9)), LocalDate.of(2008, 11, 1)
+        bookArray[3] = new Book("Blue Marvel", p1, LocalDate.of(2008, 11, 1)
                         , "super hero");
-        bookArray[4] = new Book("Icon", new Person("Dwayne", "McDuffie", LocalDate
-                        .of(1962, 2, 20)), LocalDate.of(1993, 5, 1)
+        bookArray[4] = new Book("Icon", p1, LocalDate.of(1993, 5, 1)
                         , "super hero");
 
         System.out.println(getNewestBook(bookArray));
         printYoungestAuthor(bookArray);
         System.out.println();
         printSortedByTitle(bookArray);
+        System.out.println();
+        countBooksPerAuthor(bookArray);
     }
 
     public static Book getNewestBook(Book[] books){
@@ -56,5 +59,15 @@ public class BookApp {
         Stream.of(books)
                 .sorted(Comparator.comparing(Book::getTitle))
                 .forEach(System.out::println);
+    }
+
+    public static void countBooksPerAuthor(Book[] books) {
+
+        Set<Person> authors = Stream.of(books)
+                .map(Book::getAuthor)
+                .collect(Collectors.toSet());
+
+        authors.forEach(e -> System.out.println("Author: " + e.getFirstName() + " " + e
+                .getLastName() + " - Books: " + Stream.of(books).filter(b -> b.getAuthor().equals(e)).count()));
     }
 }
